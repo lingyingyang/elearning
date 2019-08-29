9d8db769780afaedf0735c987a7906c46ae989d5
lingyingyang <41569566+lingyingyang@users.noreply.github.com>
2019-08-29 19:39:14 +1000

    remove redundancy of models in users

--- users/models.py
+++ users/models.py
@@ -2,29 +2,31 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+# name & email are in auth_user
 class Lecturer(models.Model):
-    name = models.CharField(max_length=80, blank=True, null=True)
+    # name = models.CharField(max_length=80, blank=True, null=True)
     account = models.ForeignKey(User, models.DO_NOTHING, db_column='account', blank=True, null=True)
-    email = models.CharField(max_length=80, blank=True, null=True)
+    # email = models.CharField(max_length=80, blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'lecturer'
     
     def __str__(self):
-        return f'Lecturer {self.name}'
+        return f'Lecturer {self.account.username}'
 
 
+# name, email, last_login are in auth_user
 class Student(models.Model):
-    name = models.CharField(max_length=80)
+    # name = models.CharField(max_length=80)
     gender = models.CharField(max_length=80, blank=True, null=True)
     nationality = models.CharField(max_length=80, blank=True, null=True)
     occupy = models.CharField(max_length=80, blank=True, null=True)
     graduated_university = models.CharField(max_length=80, blank=True, null=True)
-    email = models.CharField(max_length=80, blank=True, null=True)
+    # email = models.CharField(max_length=80, blank=True, null=True)
     account = models.ForeignKey(User, models.DO_NOTHING, db_column='account', blank=True, null=True)
     course = models.ForeignKey('course.Course', models.DO_NOTHING, db_column='course', blank=True, null=True)
-    last_login = models.DateTimeField(blank=True, null=True)
+    # last_login = models.DateTimeField(blank=True, null=True)
     accumulated_online_time = models.DateTimeField(blank=True, null=True)
     dob = models.CharField(max_length=80, blank=True, null=True)
 
@@ -33,4 +35,4 @@ class Student(models.Model):
         db_table = 'student'
 
     def __str__(self):
-        return f'Student {self.name}'
+        return f'Student {self.account.username}'
