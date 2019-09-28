9079b9f49b48bd1d1039e5bf38db10bca3170b0c
lingyingyang <41569566+lingyingyang@users.noreply.github.com>
2019-09-28 15:48:28 +1000

    refactored service

--- course/urls.py
+++ course/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('about/', views.about, name='course-about'),
     path('courses/', views.courses_list, name='course-courses'),
     path('courses-cb/', views.courses_cb, name='course-cb'),
-    path('cbtest/<int:subject>', views.cbtest, name='cb-test'),
     path('courses-single/<int:course_id>', views.course_single, name='courses-single'),
     path('course-enroll', views.course_enroll, name='course-enroll'),
     path('course-dismiss', views.course_dismiss, name='course-dismiss'),
